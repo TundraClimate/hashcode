d87f32345cd4d400427925f59238317917a05517
TundraClimate <tundra.tundere@gmail.com>
2023-06-10 23:42:05 +0900

    init

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+struct HashCode<T: Hash>(T);
+
+impl<T: Hash> Into<u64> for HashCode<T> {
+    fn into(self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        self.0.hash(&mut hasher);
+        hasher.finish()
+    }
+}
+
+impl<T: Hash> HashCode<T> {
+    pub fn into_u64(self) -> u64 {
+        self.into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn hash_num() {
+        let hash = HashCode(1243).into_u64();
+        assert_ne!(15130871412783076140, hash);
+    }
+
+    #[test]
+    fn hash_str_literal() {
+        let hash = HashCode("Hello, World").into_u64();
+        assert_ne!(15130871412783076140, hash);
+    }
+}
